0022686ab6b33b025af8ab9ab37120864cb0b68d
bahamin hosseinpor <bahamin.hosseinpoor@gmail.com>
2025-05-09 10:01:38 +0330

    end of project

---
+++ order.js
@@ -0,0 +1,20 @@
+const Orders = [
+  {
+    productName: "JavaScript Tutorial",
+    productNumber: "85743",
+    paymentStatus: "Due",
+    status: "Pending",
+  },
+  {
+    productName: "CSS Full Course",
+    productNumber: "95286",
+    paymentStatus: "Refunded",
+    status: "Declined",
+  },
+  {
+    productName: "Flex-Box Tutorial",
+    productNumber: "2354",
+    paymentStatus: "Paid",
+    status: "Active",
+  },
+];
